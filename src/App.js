e2e9404f84e12229f41418b9a2ff5a29e4cc411c
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2024-06-01 05:15:58 +0100

    Add main component structure update

--- src/App.js
+++ src/App.js
@@ -1,51 +1,17 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col, Alert } from "react-bootstrap";
-import FlightForm from "./FlightForm";
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import { useGlobalState } from "./context/GlobalStateContext";
+import React from 'react';
+import {Container} from 'react-bootstrap';
+import Footer from './components/Footer';
+import AppRouter from './Routes/AppRouter';
+import {GlobalStateProvider} from './context/GlobalStateContext';
 
 function App() {
-  const { leaveTime } = useGlobalState();
-
   return (
     <Container className="mt-4">
-      <Row className="justify-content-center">
-        <Col xs={12} className="text-center">
-          <h1>What time should I leave for the airport?</h1>
-        </Col>
-      </Row>
-      <Row className="justify-content-center">
-        <Col xs={12}>
-          <div className="border p-4 rounded shadow-sm bg-light">
-            <Routes>
-              <Route path="/" element={<FlightForm />} />
-            </Routes>
-            {leaveTime && (
-              <Alert variant="info" className="text-center mt-3">
-                {leaveTime}
-              </Alert>
-            )}
-          </div>
-        </Col>
-      </Row>
-      <footer className="footer mt-4">
-        <p className="text-center small">
-          a silly&nbsp;
-          <a
-            href="https://github.com/mattsilv/airport-time-estimator"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            open-source
-          </a>{" "}
-          app by{" "}
-          <a href="https://silv.blog" target="_blank" rel="noopener noreferrer">
-            silv.eth
-          </a>
-        </p>
-      </footer>
+      <GlobalStateProvider>
+        <AppRouter />
+      </GlobalStateProvider>
+
+      <Footer />
     </Container>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { GlobalStateProvider } from "./context/GlobalStateContext";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <GlobalStateProvider>
-    <Router>
-      <App />
-    </Router>
-  </GlobalStateProvider>
-);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
