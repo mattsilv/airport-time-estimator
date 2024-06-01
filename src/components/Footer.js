f4d709cc260d5c05e49a4fcb71bac300daaf6f0a
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2024-06-01 05:13:16 +0100

    Add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer mt-4">
+      <p className="text-center small">
+        a silly&nbsp;
+        <a
+          href="https://github.com/mattsilv/airport-time-estimator"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-source
+        </a>{' '}
+        app by{' '}
+        <a href="https://silv.blog" target="_blank" rel="noopener noreferrer">
+          silv.eth
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
